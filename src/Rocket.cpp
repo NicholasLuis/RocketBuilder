6e80015f8bb3246b815ab9ebee09a5cdcbde931a
Dominic-M1213 <djm7601@psu.edu>
2024-04-11 15:25:56 -0400

    Pulled Rocket Class from NicksBranch

--- src/Rocket.cpp
+++ src/Rocket.cpp
@@ -1,9 +1,80 @@
 #include "Rocket.h"
 
-// -----TOTAL ROCKET CLASS-----
-void TotalRocket::addToRocket(RocketParts* rocketPart2Add)
+// -----TOTAL STAGE CLASS-----
+RocketStage::RocketStage(double structW, double fuelW, double specImp)
 {
-	TotalRocket.insert(TotalRocket.begin(), rocketPart2Add); // Adds a rocket part to the total rocket
+	stageStructureMass = structW;
+	stageFuelMass = fuelW;
+	stageTotalMass = stageStructureMass + stageFuelMass;
+
+	totalMass += stageTotalMass;
+
+	I_sp = specImp;
+}
+RocketStage::~RocketStage()
+{
+	totalMass -= stageStructureMass; // Assumes that all the fuel has been spent before stage detaches
+}
+double RocketStage::getFuelMass()
+{
+	return stageFuelMass;
+}
+double RocketStage::getStructureMass()
+{
+	return stageStructureMass;
+}
+double RocketStage::getTotalMass()
+{
+	return stageTotalMass;
+}
+double RocketStage::getI_sp()
+{
+	return I_sp;
+}
+void RocketStage::setMutex(std::mutex* mutex)
+{
+	printMutex = mutex;
 }
 
-void getDeltaV
+// -----TOTAL ROCKET CLASS-----
+void TotalRocket::addToRocket(RocketStage* rocketPart2Add)
+{
+	totalRocketQueue.push(rocketPart2Add); // Adds a stage to the end
+}
+double TotalRocket::getDeltaV() // this calculates the delta V if you burn all the fuel from all of the remaing stages
+{
+	// makes a temporary copy of the RocketQueue and pops it so that it doesn't delete the rocket after doing the 
+	std::queue<RocketStage*> copyOfRocketQueue = totalRocketQueue;
+	double totalDeltaV = 0, stageDeltaV = 0;
+
+	double stageISP, initialMass, finalMass, logMassRatio;
+
+	for (int i = 0; i < copyOfRocketQueue.size(); i++)
+	{
+		stageISP = copyOfRocketQueue.front()->getI_sp();
+		initialMass = copyOfRocketQueue.front()->getTotalMass();
+		finalMass = copyOfRocketQueue.front()->getStructureMass(); // final mass of the stage is just the structure (all fuel burnt)
+		logMassRatio = std::log(initialMass / finalMass);
+		stageDeltaV = stageISP * g * logMassRatio;
+
+		totalDeltaV += stageDeltaV;
+		// Calculates the delta V of each stage
+		copyOfRocketQueue.pop(); // Does not delete the objects inside since it only contains pointers
+
+	}
+}
+// OPTIONAL FEATURE: COME BACK TO THIS ONCE WE'RE DONE WITH THE PROJECT
+//double TotalRocket::getDeltaV( double fuelToBurn ) // this calculates the delta V if you only burn a certain amount of fuel
+//{
+//
+//}
+void TotalRocket::detatchStage()
+{
+	totalRocketQueue.front()->~RocketStage();	// 1. Delete stage object (call de-constructor)
+	totalRocketQueue.pop();						// 2. remove stage from the queue
+
+}
+void TotalRocket::setMutex(std::mutex* mutex)
+{
+	printMutex = mutex;
+}
--- src/Rocket.h
+++ src/Rocket.h
@@ -3,71 +3,62 @@
 #ifndef Rocket_H
 #define Rocket_H
 
-#include <vector>
+#include <queue>
 #include "global.h"
+#include <cmath> // needed to take natural log for deltaV calculations
+#include <mutex>
 
-// Rocket Parts
-class RocketParts // parent class: each rocket part should have these quantities
-{
+class Rocket // Each rocket stage share these quantities
+{			// This class is only a template for other classes to use
+protected:
+	double totalMass; // total mass of the whole rocket
+	double fuelMass; // total amount of fuel in the whole rocket
+	double structureMass;
+	std::mutex* printMutex = new std::mutex; // Mutex that controls print to console (a shared resource)
+
 public:
-	double getWeight(); // Returns the weight of the part
+	virtual double getFuelMass() = 0;
+	virtual double getStructureMss() = 0;
+	virtual double getTotalMass() = 0;
+	virtual void setMutex(std::mutex* mutex) = 0;
 };
 
-class engines : public RocketParts // the engines are one of the pieces of the rocket
-{
-private:
-	double thrust;
-	double weight;
-public:
 
-};
-class booster : public RocketParts
-{
-	double structuralWeight;
-	double fuelWeight;
-	double I_sp; // specific impulse
-public:
-	engines(double Thrust, double StructWeight, double FuelWeight, double I_sp); // Default constructor for building unique part
-};
-
-class booster : public RocketParts
+class RocketStage : public Rocket // Each stage is a rocket in of itself
 {
 private:
-	double thrust;
-	double structuralWeight;
-	double fuelWeight;
-	double I_sp; // specific impulse
+	double stageTotalMass; // this is the mass of the stage
+	double stageFuelMass;
+	double stageStructureMass;
+	double I_sp;
+
 public:
-	booster(double Thrust, double StructWeight, double FuelWeight, double I_sp); // Default constructor for building unique part
+	RocketStage(double structW, double fuelW, double I_sp);
+	~RocketStage(); // de-constructor
+	double getFuelMass();
+	double getStructureMass();
+	double getTotalMass();
+	double getI_sp();
+	virtual void setMutex(std::mutex* mutex);
 };
+
 
 // Total Rocket
-class TotalRocket 
+class TotalRocket : public Rocket // The total rocket is obviously also a rocket
 {
 private:
-
-	std::vector<RocketParts*> RocketPieces; // A vector that pieces together the individual components of the rocket (Can also hold objects derived from RocketParts class)
-	
-	engines* engine = new engines;
-
-
-public:
-	double getFuelWeight(); // Returns the weight of the fuel
-	double getTotalWeight(); // Returns the total weight of the rocket
-
-	void addEngine(engines* engine); 
-	std::vector<RocketParts*> TotalRocket; 
-	// ^ A vector that pieces together the individual components of the rocket (Can also hold objects derived from RocketParts class)
-	// Note: Build the rocket from top->bottom
+	std::queue<RocketStage*> totalRocketQueue;
+	// ^ A list that pieces together the individual components of the rocket (Can also hold objects derived from RocketParts class)
+	// Note: Build the rocket from bottom->top
 
 public:
 
-	double getFuelWeight(); // Returns the weight of the fuel
-	double getTotalWeight(); // Returns the total weight of the rocket
+	void addToRocket(RocketStage* rocketPart2Add);
+	void detatchStage(); // Detaches the bottom stage (obviously)
+	double getDeltaV(); // Returns the delta v if you burn all the fuel
+	//	double getDeltaV(double fuelToBurn); // Returns the delta v if you burn all the fuel
+	virtual void setMutex(std::mutex* mutex);
 
-	void addToRocket(RocketParts* rocketPart2Add); 
-	void stageSeparation();
-	void getDeltaV();
 };
 
 #endif
