5e5472565d582aad80b292a9044a25909ea46b01
Dominic-M1213 <djm7601@psu.edu>
2024-04-11 11:14:23 -0400

    Updated Rocket.h

--- src/Rocket.h
+++ src/Rocket.h
@@ -4,6 +4,10 @@
 #define Rocket_H
 
 #include <vector>
+<<<<<<< HEAD
+=======
+#include "global.h"
+>>>>>>> origin/NicksBranch
 
 // Rocket Parts
 class RocketParts // parent class: each rocket part should have these quantities
@@ -11,10 +15,15 @@ class RocketParts // parent class: each rocket part should have these quantities
 public:
 	double getWeight(); // Returns the weight of the part
 };
+<<<<<<< HEAD
+=======
+
+>>>>>>> origin/NicksBranch
 class engines : public RocketParts // the engines are one of the pieces of the rocket
 {
 private:
 	double thrust;
+<<<<<<< HEAD
 	double weight;
 public:
 
@@ -22,12 +31,31 @@ public:
 class booster : public RocketParts
 {
 
+=======
+	double structuralWeight;
+	double fuelWeight;
+	double I_sp; // specific impulse
+public:
+	engines(double Thrust, double StructWeight, double FuelWeight, double I_sp); // Default constructor for building unique part
+};
+
+class booster : public RocketParts
+{
+private:
+	double thrust;
+	double structuralWeight;
+	double fuelWeight;
+	double I_sp; // specific impulse
+public:
+	booster(double Thrust, double StructWeight, double FuelWeight, double I_sp); // Default constructor for building unique part
+>>>>>>> origin/NicksBranch
 };
 
 // Total Rocket
 class TotalRocket 
 {
 private:
+<<<<<<< HEAD
 	std::vector<RocketParts*> RocketPieces; // A vector that pieces together the individual components of the rocket (Can also hold objects derived from RocketParts class)
 	
 	engines* engine = new engines;
@@ -38,6 +66,20 @@ public:
 	double getTotalWeight(); // Returns the total weight of the rocket
 
 	void addEngine(engines* engine); 
+=======
+	std::vector<RocketParts*> TotalRocket; 
+	// ^ A vector that pieces together the individual components of the rocket (Can also hold objects derived from RocketParts class)
+	// Note: Build the rocket from top->bottom
+
+public:
+
+	double getFuelWeight(); // Returns the weight of the fuel
+	double getTotalWeight(); // Returns the total weight of the rocket
+
+	void addToRocket(RocketParts* rocketPart2Add); 
+	void stageSeparation();
+	void getDeltaV();
+>>>>>>> origin/NicksBranch
 };
 
 #endif
