c436cf411573aeec67ca409dc4514969c89590a0
NicholasLuis <nml5604@psu.edu>
2024-04-11 12:41:18 -0400

    Got inheritance down via rocket classes

--- src/Rocket.cpp
+++ src/Rocket.cpp
@@ -1,9 +1,48 @@
 #include "Rocket.h"
 
-// -----TOTAL ROCKET CLASS-----
-void TotalRocket::addToRocket(RocketParts* rocketPart2Add)
+// -----ENGINES CLASS-----
+//engines::engines(double Thrust, double StructWeight, double FuelWeight, double specImp)
+//{
+//	thrust = Thrust;
+//	structuralWeight = StructWeight;
+//	fuelWeight = FuelWeight;
+//	I_sp = specImp;
+//}
+//double engines::getStructureWeight()
+//{
+//
+//}
+
+// -----TOTAL STAGE CLASS-----
+RocketStage::RocketStage(double structW, double fuelW)
 {
-	TotalRocket.insert(TotalRocket.begin(), rocketPart2Add); // Adds a rocket part to the total rocket
+	stageStructureWeight = structW;
+	stageFuelWeight = fuelW;
+	stageTotalWeight = stageStructureWeight + stageFuelWeight;
+
+	totalWeight += stageTotalWeight;
+}
+double RocketStage::getFuelWeight()
+{
+	return stageFuelWeight;
 }
 
-void getDeltaV
+// -----TOTAL ROCKET CLASS-----
+void TotalRocket::addToRocket(RocketStage* rocketPart2Add)
+{
+	totalRocketVector.insert(totalRocketVector.begin(), rocketPart2Add); // Adds a rocket stage to the total rocket
+}
+
+double TotalRocket::getDeltaV() // this calculates the delta V if you burn all the fuel
+{
+	double totalRocketWeight = 0;
+	double totalFuelWeight = 0;
+	for (int i = 0; i < totalRocketVector.size(); i++)
+	{
+		totalRocketWeight += totalRocketVector[i]->getStructureWeight();
+
+		totalRocketWeight += totalRocketVector[i]->getFuelWeight();
+		totalFuelWeight += totalRocketVector[i]->getFuelWeight();
+	}
+
+}
--- src/Rocket.h
+++ src/Rocket.h
@@ -6,40 +6,39 @@
 #include <vector>
 #include "global.h"
 
-// Rocket Parts
-class RocketParts // parent class: each rocket part should have these quantities
-{
-public:
-	double getWeight(); // Returns the weight of the part
+
+class Rocket // Each rocket stage share these quantities
+{			// This class is only a template for other classes to use
+protected:
+	double totalWeight; // total weight of the rocket
+	double fuelWeight; // total amount of fuel in the rocket
+	double structureWeight;
+
+	virtual double getTotalWeight();
+	virtual double getFuelWeight();
+	virtual double getStructureWeight();
 };
 
-class engines : public RocketParts // the engines are one of the pieces of the rocket
+
+class RocketStage : public Rocket // Each stage is a rocket in of itself
 {
-private:
-	double thrust;
-	double structuralWeight;
-	double fuelWeight;
-	double I_sp; // specific impulse
+private: 
+	double stageTotalWeight; // this is the weight of the stage
+	double stageFuelWeight;
+	double stageStructureWeight;
+
 public:
-	engines(double Thrust, double StructWeight, double FuelWeight, double I_sp); // Default constructor for building unique part
+	RocketStage(double structW, double fuelW);
+	double getFuelWeight();
+	double getTotalWeight();
 };
 
-class booster : public RocketParts
-{
-private:
-	double thrust;
-	double structuralWeight;
-	double fuelWeight;
-	double I_sp; // specific impulse
-public:
-	booster(double Thrust, double StructWeight, double FuelWeight, double I_sp); // Default constructor for building unique part
-};
 
 // Total Rocket
-class TotalRocket 
+class TotalRocket : public Rocket // The total rocket is obviously also a rocket
 {
 private:
-	std::vector<RocketParts*> TotalRocket; 
+	std::vector<RocketStage*> totalRocketVector; 
 	// ^ A vector that pieces together the individual components of the rocket (Can also hold objects derived from RocketParts class)
 	// Note: Build the rocket from top->bottom
 
@@ -48,9 +47,57 @@ public:
 	double getFuelWeight(); // Returns the weight of the fuel
 	double getTotalWeight(); // Returns the total weight of the rocket
 
-	void addToRocket(RocketParts* rocketPart2Add); 
-	void stageSeparation();
-	void getDeltaV();
+	void addToRocket(RocketStage* rocketPart2Add); 
+	double getDeltaV(); // Returns the delta v if you burn all the fuell
 };
+
+//// Rocket Parts
+//class RocketParts // parent class: each rocket part should have these quantities
+//{
+//public:
+//	virtual double getStructureWeight(); // Returns the weight of the part
+//	virtual double getFuelWeight();
+//};
+//
+//class engines : public RocketParts // the engines are one of the pieces of the rocket
+//{
+//private:
+//	double thrust;
+//	double structuralWeight;
+//	double fuelWeight;
+//	double I_sp; // specific impulse
+//public:
+//	engines(double Thrust, double StructWeight, double FuelWeight, double specImp); // Default constructor for building unique part
+//	double getFuelWeight();
+//	double getStructureWeight();
+//};
+//
+//class booster : public RocketParts
+//{
+//private:
+//	double thrust;
+//	double structuralWeight;
+//	double fuelWeight;
+//	double I_sp; // specific impulse
+//public:
+//	booster(double Thrust, double StructWeight, double FuelWeight, double I_sp); // Default constructor for building unique part
+//};
+//
+//// Total Rocket
+//class TotalRocket 
+//{
+//private:
+//	std::vector<RocketParts*> totalRocketVector; 
+//	// ^ A vector that pieces together the individual components of the rocket (Can also hold objects derived from RocketParts class)
+//	// Note: Build the rocket from top->bottom
+//
+//public:
+//
+//	double getFuelWeight(); // Returns the weight of the fuel
+//	double getTotalWeight(); // Returns the total weight of the rocket
+//
+//	void addToRocket(RocketParts* rocketPart2Add); 
+//	double getDeltaV(); // Returns the delta v if you burn all the fuell
+//};
 
 #endif
