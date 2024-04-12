93b7ce994aeec4cbb8b3530b3eecb519a1ca9796
NicholasLuis <nml5604@psu.edu>
2024-04-12 01:10:42 -0400

    Working on orbit equations

--- src/orbit.cpp
+++ src/orbit.cpp
@@ -1,3 +1,44 @@
 # include <../src/orbit.h>
 
-Orbit::Orbit() {}
+Orbit::Orbit() // default constructor
+{
+	// Kennedy Space Center is the default launch site
+	launchCoords.push_back(28.5744); // Lattitude ( >0 for northern hemisphere and <0 for southern hemisphere)
+	launchCoords.push_back(80.6520); // Longitude
+	initialVelo *= cos(launchCoords[0]); // Adjusts initial velocity depending on the lattitude
+}
+void Orbit::launchPossibilities(double deltaV) // Possible orbit radii depending on available delta V
+{
+	
+}
+void Orbit::inclinationPossibilities() // Possible launch inclinations from earth
+{
+	if (launchCoords[0] > 0) // If the rocket is launching from the northern hemisphere
+	{
+		toPrint = "The rocket can launch into inclinations between " + std::to_string(launchCoords[1]) +
+				  " degrees and 90 degrees";
+	}
+	else if (launchCoords[0] < 0) // If the rocket is launching from the souther hemisphere
+	{
+		toPrint = "The rocket can launch into inclinations between " + std::to_string(launchCoords[1]) +
+				  " degrees and -90 degrees";
+	}
+	else // The rocket is launching from the equator
+	{
+		toPrint = "The rocket can only launch into an inclination of 0 degrees or 180 degrees";
+	}
+}
+
+void Orbit::inclinationPossibilities(double deltaV, double velocity) // must enter velocity as it crosses the equitorial plane
+{
+	double delta_i = 2.0 * asin(0.5 * deltaV / velocity) * (180/PI);
+	toPrint = "The satellite can change its inclination by " + std::to_string(delta_i) + " degrees";
+}
+
+void Orbit::inclinationPossibilities(double deltaV, double radius) // radius assuming circular orbit
+{
+	double velocity = sqrt(MU / (radius * radius));
+	double delta_i = 2.0 * asin(0.5 * deltaV / velocity) * (180 / PI);
+	toPrint = "The satellite can change its inclination by " + std::to_string(delta_i) + " degrees";
+}
+
--- src/orbit.h
+++ src/orbit.h
@@ -1,6 +1,24 @@
 # pragma once
+#include <vector>
+#include <string>
+#include <cmath>
 
-class Orbit {
+const double PI = 3.141592653;
+const double MU = 3.986 * (10 ^ 5);// G * M_earth
+
+// This class controls the orbit stuff
+// For example: given the initial position and deltaV, it will output possible orbits depending on user input
+class Orbit { // Always assume perfectly circular orbit
+private: 
+	double initialVelo = ; // Initial velocity = tangential velocity on equator
+	std::vector<double> launchCoords; // Initializes a vector of size 2 (lat and long coordinates)
+	std::string toPrint;
 public:
-	Orbit();
+	Orbit(); // Default constructor: assumes that the rocket is launching from earth
+
+	void launchPossibilities(double deltaV);
+	void inclinationPossibilities(); // Launching from earth
+	void inclinationPossibilities(double deltaV, double velocity); // Inclination change (already in orbit)
+	void inclinationPossibilities(double deltaV, double radius); // Inclination change (already in orbit)
+
 };
