0fca833463d7e352dfb7971139d9ab2b78792053
Dominic-M1213 <djm7601@psu.edu>
2024-04-13 12:19:48 -0400

    Adding Comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,31 @@
 # include <../src/global.h>
 
+// Log Function to Control Log Messages
 void log(const std::string& str) {
     ConsoleManager::getInstance().log(str);
 }
 
+
 int main() {
-    GuiManager guiManager;
-    ConsoleManager::getInstance().run();
-    guiManager.start();
+    GuiManager guiManager;                          // Create GuiManager object
+    ConsoleManager::getInstance().run();			// Run ConsoleManager
+    guiManager.start();							    // Start GUI
   
-    std::thread t1([] { log("Hello from T1"); });
-    std::thread t2([] { log("Hello from T2"); });
-    std::thread t3([] { log("Hello from T3"); });
-    std::thread t4([] { log("Hello from T4"); }); 
+    // create threads to check console log
+    std::thread t1([] { log("Checking Console Log... [1]"); });
+    std::thread t2([] { log("Checking Console Log... [2]"); });
+    std::thread t3([] { log("Checking Console Log... [3]"); });
+    std::thread t4([] { log("Checking Console Log... [4]"); });
     t1.join();
     t2.join();
     t3.join();
     t4.join();
 
-    while (guiManager.isRunning()) {
+    while (guiManager.isRunning()) {    
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
-    guiManager.stop();
-    ConsoleManager::getInstance().stop();
+    guiManager.stop(); 							    // Stop GUI
+    ConsoleManager::getInstance().stop();			// Stop ConsoleManager
     return 0;
 }
